ee00e6cdeb739129fb32286effcce10ea5e0761e
Hosshii <sao_heath6147.wistre@icloud.com>
2021-05-16 00:26:19 +0900

    fix copy dir

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,9 @@
 use clap::{self, App, Arg, SubCommand};
+use fs_extra::dir::{self, CopyOptions};
 use std::{env, fs, path::Path, process::exit};
 
-// struct MyApp {
-//     cmd: String,
-//     _sub_cmd: Vec<MyApp>,
-//     _flag: Vec<String>,
-//     _options: Vec<String>,
-// }
-
-// impl MyApp {
-//     fn new(cmd_name: impl Into<String>) -> Self {
-//         Self {
-//             cmd: cmd_name.into(),
-//             _sub_cmd: Vec::new(),
-//             _flag: Vec::new(),
-//             _options: Vec::new(),
-//         }
-//     }
-//     fn sub_cmd(mut self, a: MyApp) -> Self {
-//         self._sub_cmd.push(a);
-//         self
-//     }
-//     fn option(mut self, a: impl Into<String>) -> Self {
-//         self._options.push(a.into());
-//         self
-//     }
-// }
-
 const SATYSFI: &str = "saty";
+const SATY_DIR: &str = "dir";
 const TEMPLATE_DIR: &str = "TOOLBOX_TEMPLATE_DIR";
 
 fn main() {
@@ -35,54 +11,55 @@ fn main() {
     // let a = "~/.template".to_string();
 
     let matches = app.get_matches();
-    if let Some(target_dir_str) = matches.value_of(SATYSFI) {
-        let template_dir_str =
-            env::var(TEMPLATE_DIR).expect(&format!("{} is not set", TEMPLATE_DIR));
-        let template_dir_str = format!("{}/satysfi", template_dir_str);
-        let template_dir = Path::new(&template_dir_str);
-        if !template_dir.exists() {
-            eprintln!("{} does not exists", template_dir_str);
-            exit(1);
-        }
 
-        let target_dir = Path::new(target_dir_str);
-        if target_dir.exists() {
-            eprintln!("{} already exist", target_dir_str);
-            exit(1);
-        }
-        match fs::create_dir(target_dir) {
-            Ok(_) => (),
-            Err(e) => {
-                eprintln!("err {}", e);
+    if let Some(args) = matches.subcommand_matches(SATYSFI) {
+        if let Some(target_dir_str) = args.value_of(SATY_DIR) {
+            dbg!();
+            let template_dir_str =
+                env::var(TEMPLATE_DIR).expect(&format!("{} is not set", TEMPLATE_DIR));
+            let template_dir_str = format!("{}/satysfi/", template_dir_str);
+            let template_dir = Path::new(&template_dir_str);
+            if !template_dir.exists() {
+                eprintln!("{} does not exists", template_dir_str);
                 exit(1);
             }
-        }
 
-        match fs::copy(template_dir, target_dir) {
-            Ok(_) => {
-                println!(
-                    "successfully copied from {} to {}",
-                    template_dir_str, target_dir_str
-                )
-            }
-            Err(e) => {
-                eprintln!("err {}", e);
+            let target_dir = Path::new(target_dir_str);
+            if target_dir.exists() {
+                eprintln!("{} already exist", target_dir_str);
                 exit(1);
             }
-        }
-    };
+
+            println!("{:?}", target_dir);
+            match fs::create_dir(target_dir) {
+                Ok(_) => (),
+                Err(e) => {
+                    dbg!("{}", template_dir);
+                    dbg!("{}", target_dir);
+                    eprintln!("err {}", e);
+                    exit(1);
+                }
+            }
+            let mut options = CopyOptions::new();
+            options.content_only = true;
+
+            match dir::copy(template_dir, target_dir, &options) {
+                Ok(_) => {
+                    println!(
+                        "successfully copied from {} to {}",
+                        template_dir_str, target_dir_str
+                    )
+                }
+                Err(e) => {
+                    dbg!("{}", template_dir);
+                    dbg!("{}", target_dir);
+                    eprintln!("err {}", e);
+                    exit(1);
+                }
+            }
+        };
+    }
 }
-
-// fn new_my_app() -> MyApp {
-//     // let satysfi = MyApp {
-//     //     cmd: "satysfi".to_owned(),
-//     //     sub_cmd: Vec::new(),
-//     //     flag: Vec::new(),
-//     //     options: vec!["dir".to_owned()],
-//     // };
-//     let myapp = MyApp::new("toolbox");
-//     myapp
-// }
 
 fn new_app<'a, 'b>() -> App<'a, 'b> {
     let app = App::new("toolbox")
@@ -93,7 +70,7 @@ fn new_app<'a, 'b>() -> App<'a, 'b> {
             SubCommand::with_name(SATYSFI)
                 .about("create satysfi template")
                 .arg(
-                    Arg::with_name("dir")
+                    Arg::with_name(SATY_DIR)
                         .help("directory where make template")
                         .required(true),
                 ),
